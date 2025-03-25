b2aabc487c67dd3a846e15be7cd63b11fa11b103
Divekkamboj <divekkamboj791@gmail.com>
2025-03-25 14:29:08 +0000

    container config done

---
+++ 01_Basics/03_Conversion.js
@@ -0,0 +1,77 @@
+//let score = 33
+
+//console.log(typeof score);
+//console.log(typeof(score));  /* Output: number number */
+                                        
+
+//let score = "33abc"
+
+//console.log(typeof score);
+//console.log(typeof(score));  /* output: string string */
+
+//let valueInNumber = Number(score) 
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);  /* output: NaN(Not a Number) */
+
+//let score = null
+//console.log(typeof score);
+//console.log(typeof(score));  /* output: object object */
+
+//let valueInNumber = Number(score) 
+//console.log(typeof valueInNumber);  /* output: number */
+//console.log(valueInNumber);  /* output: 0 */
+
+//let score = undefined
+//console.log(typeof score);
+//console.log(typeof(score));  /* output: undefined undefined */
+
+//let valueInNumber = Number(score) 
+//console.log(typeof valueInNumber);  /* output: number */
+//console.log(valueInNumber);  /* output: NaN */
+
+//let score = true
+//console.log(typeof score);
+//console.log(typeof(score));  /* output: Boolean Boolean */
+
+//let valueInNumber = Number(score) 
+//console.log(typeof valueInNumber);  /* output: number */
+//console.log(valueInNumber);  /* 1 */
+
+//let score = Hitesh
+//console.log(typeof score);
+//console.log(typeof(score));  /* output: string string */
+
+//let valueInNumber = Number(score) 
+//console.log(typeof valueInNumber);  /* output: number */
+//console.log(valueInNumber);  /* NaN */
+
+//let isloggedIn = 1
+
+//let booleanIsloggedIn = Boolean(isloggedIn)
+//console.log(booleanIsloggedIn); // Output: true
+
+//let isloggedIn = "Hitesh"
+
+//let booleanIsloggedIn = Boolean(isloggedIn)
+//console.log(booleanIsloggedIn); // Output: true
+
+//let isloggedIn = 0
+
+//let booleanIsloggedIn = Boolean(isloggedIn)
+//console.log(booleanIsloggedIn); // Output:  false
+
+//let isloggedIn = ""
+
+//let booleanIsloggedIn = Boolean(isloggedIn)
+//console.log(booleanIsloggedIn); //Output: false
+
+//let someNumber = 33 
+
+//let stringNumber = String(someNumber)
+//console.log(stringNumber);  // output: 33
+
+//let someNumber = 33
+
+//let stringNumber = String(someNumber)
+//console.log(stringNumber); 
+//console.log(typeof stringNumber); // Output 33  string
